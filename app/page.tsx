63abc89e63049150cde76bf36588f82490a1ce72
Mohit Raut <mohitraut.21@stvincentngp.edu.in>
2024-11-05 15:18:09 +0530

    initialize Next.js project with Tailwind CSS and essential configurations

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="container">
+      <h1>Hello Master!!!</h1>
+      <Button variant="destructive">Hila</Button>
+    </div>
+  );
+}
